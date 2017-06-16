5cbd526b623ab989e935debbc75857bf4b5d5a00
shinate <shine.wangrs@gmail.com>
2017-06-16 14:06:10 +0800

    gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 'use stick';
 
+var args = require('yargs').argv;
 var gulp = require('gulp');
 var gulpMulDest = require('gulp-multi-dest');
 var rename = require('gulp-rename');
@@ -23,7 +24,27 @@ function getProductions() {
 }
 
 gulp.task('sync', function () {
-    return gulp.src('gulpfile-product-default.js')
-        .pipe(rename('gulpfile.js'))
-        .pipe(gulpMulDest(getProductions()));
+    var type = args.f;
+    switch (type) {
+        case 'gulpfile':
+            return gulp.src('./Templates/production-directory-example/gulpfile.js')
+                .pipe(gulpMulDest(getProductions()));
+        case 'ignore':
+            return gulp.src('./Templates/production-directory-example/.gitignore')
+                .pipe(gulpMulDest(getProductions()));
+    }
+});
+
+gulp.task('init', function () {
+
+    var dest = args.d;
+
+    if (dest && fs.lstatSync(dest).isDirectory()) {
+        return gulp.src('./Templates/production-directory-example/**/*')
+            .pipe(gulp.dest(dest));
+    }
+
+    console.error('Not a valid directory');
+
+    return false;
 });
